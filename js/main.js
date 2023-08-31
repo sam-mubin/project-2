f7d2400ce639d61959d4923065f64899313332ee
sam-mubin <sheikhabdulmubin@gmail.com>
2023-08-31 22:20:40 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// NAVIGATION
+
+function openMenu(){
+    document.getElementById("mainmenu").style.width = "100%";
+}
+
+function closeMenu(){
+    document.getElementById("mainmenu").style.width = "0";
+}
